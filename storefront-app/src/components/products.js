84571857e40b8231a2a5691f1802749c674034ac
raghadanees <raghad.anees@hotmail.com>
2020-10-27 07:49:33 +0300

    init

--- storefront-app/src/components/products.js
+++ storefront-app/src/components/products.js
@@ -2,11 +2,55 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { chooseList } from '../store/products.js';
 
+import { Box, CardMedia, Container, Grid, Card, CardContent, CardActions, Button, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+// const useStyles = makeStyles((theme) => ({
+//     '@global': {
+//         ul: {
+//             margin: 0,
+//             padding: 0,
+//             listStyle: 'none',
+//         },
+//     },
+//     appBar: {
+//         borderBottom: `1px solid ${theme.palette.divider}`,
+//     },
+//     toolbarTitle: {
+//         flex: 1,
+//     },
+//     fullHeight: {
+//         height: "100%"
+//     },
+//     card: {
+//         margin: '1em',
+//     },
+//     media: {
+//         height: 0,
+//         paddingTop: '56.25%',
+//         borderTopLeftRadius: '5px',
+//         borderTopRightRadius: '5px'
+//     },
+//     jss8: {
+//         height: '100%',
+//         display: 'flex',
+//         flexDirection: 'column',
+//         flexWrap: 'initial'
+//     },
+//     jss5: {
+//         padding: '64px 0px 48px'
+//     },
+//     jss7: {
+//         paddingTop: '64px',
+//         paddingBottom: '64px'
+//     }
+// }));
+
 const Status = props => {
     console.log('props??????????',props);
     console.log('props.list.results??????????',props.list.results);
 
-    // console.log('props list .results????',props.list.results.name);
+// const classes = useStyles();
 
     return (
         <>
--- storefront-app/src/store/products.js
+++ storefront-app/src/store/products.js
@@ -1,13 +1,13 @@
 let initialState = {
     products: [
-        { name: 'TV', category: 'electronics', price: 699.00 },
-        { name: 'Radio', category: 'electronics', price: 99.00 },
-        { name: 'Shirt', category: 'clothing', price: 9.00 },
-        { name: 'Socks', category: 'clothing', price: 12.00 },
-        { name: 'Dipiro', category: 'books', price: 10.99 },
-        { name: 'Katzung', category: 'books', price: 15.99 },
-        { name: 'Eggs', category: 'food', price: 1.99 },
-        { name: 'Bread', category: 'food', price: 2.39},
+        { name: 'TV', category: 'electronics', price: '699.00$' },
+        { name: 'Radio', category: 'electronics', price: '99.00$' },
+        { name: 'Shirt', category: 'clothing', price: '9.00$' },
+        { name: 'Socks', category: 'clothing', price: '12.00$' },
+        { name: 'Dipiro', category: 'books', price: '10.99$ '},
+        { name: 'Katzung', category: 'books', price: '15.99$' },
+        { name: 'Eggs', category: 'food', price: '1.99$' },
+        { name: 'Bread', category: 'food', price: '2.39$'},
     ],
     results: [],
 }
